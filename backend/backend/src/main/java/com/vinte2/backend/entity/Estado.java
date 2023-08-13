71877d3714fa67c739c695c21696836b41eb7b17
yarleicrz <yarleicruz011@gmail.com>
2023-08-13 20:10:51 -0300

    update

--- backend/backend/src/main/java/com/vinte2/backend/entity/Estado.java
+++ backend/backend/src/main/java/com/vinte2/backend/entity/Estado.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,7 +24,9 @@ public class Estado {
     private long id;
 
     private String nome;
-    private String sigla;
+    @ManyToOne
+    @JoinColumn (name="idEstado")
+    private Estado estado;
     @Temporal(TemporalType.DATE)
     private Date dataCriacao;
     @Temporal(TemporalType.DATE)
